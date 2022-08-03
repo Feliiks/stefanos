f304cfb45df79a7bdfb98a668dc1f70e93e40705
Feliiks <sobrero.ludovic@gmail.com>
2022-08-03 13:42:32 +0200

    Edit: navbar toggle

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -11,6 +11,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import api from '../../utils/api'
 import { logout } from '../../reducers/user.reducer'
 
+import MenuIcon from '@mui/icons-material/Menu';
+
 const Header = () => {
     let user = useSelector(state => state.user.value)
 
@@ -37,7 +39,9 @@ const Header = () => {
             style={{ background: "rgba(10, 25, 41, 0.72)", boxShadow: "rgb(19 47 76) 0px -1px 1px inset", backdropFilter: "blur(20px)" }}>
             <Container style={{minHeight: "60px" }}>
                 <Logo />
-                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Toggle aria-controls="navbarScroll" style={{ color: "#fff" }}>
+                    <MenuIcon />
+                </Navbar.Toggle>
                 <Navbar.Collapse id="navbarScroll">
                     <NavBar user={user} />
                     <UserBtn user={user} logoutUser={logoutUser} />
